747028e90a4d229fd0d173603cb79388dfee3bef
lightme-fan <fanilo.and@onja.org>
2022-05-26 09:40:24 +0300

    [#2] - delete login and sigup tabs, update pendeng data columns and install react-cookie and jwt-decode

--- frontend/src/Components/Main/Main.jsx
+++ frontend/src/Components/Main/Main.jsx
@@ -3,7 +3,10 @@ import "./main.scss";
 
 const Main = ({ children, isLoginPage }) => {
   return (
-    <div className="main" style={{ padding: isLoginPage ? "156px 0" : "20px" }}>
+    <div
+      className="main"
+      style={{ padding: isLoginPage ? "156px 0" : "20px", height: "92vh" }}
+    >
       {children}
     </div>
   );
