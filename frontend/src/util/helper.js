edff6bfe1e7d8a85c38a0e08f796c304131db714
lightme-fan <fanilo.and@onja.org>
2022-06-08 10:26:49 +0300

    [#35] - add duration converter function and convert duration

--- frontend/src/util/helper.js
+++ frontend/src/util/helper.js
@@ -9,3 +9,13 @@ export const removeCookie = (name) => {
       name + "=; expires=Thu, 01-Jan-1970 00:00:01 GMT;" + pathCurrent + ";";
   }
 };
+
+export const numToDuration = (num) => {
+  let h = Math.floor(num / 3600);
+  let m = Math.floor((num % 3600) / 60);
+  let s = Math.round(num % 60);
+  h = `${h === 0 ? "" : `${h}h`}`;
+  m = m === 0 ? "" : `${h && m < 10 ? `0${m}` : m}min`;
+  s = `${s === 0 ? "" : `${s < 10 ? `0${s}` : s}`}${s === 0 ? "" : "s"}`;
+  return `${h} ${m} ${s}`;
+};
