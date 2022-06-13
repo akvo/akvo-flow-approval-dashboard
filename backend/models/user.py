f055574e61f1e5d175451ca51406a9518c285f6d
dedenbangkit <mail@dedenbangkit.com>
2022-06-13 17:08:04 +0700

    [#43] Add device list to login and auth profile

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from db.connection import Base
+from .user_device import UserDevice
 
 
 class UserBase(BaseModel):
@@ -13,15 +15,19 @@ class UserBase(BaseModel):
     email_verified: bool
     is_admin: bool
     id_token: str
+    devices: List[str] = []
 
 
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True, index=True, nullable=True)
     email = Column(String, unique=True)
+    devices = relationship(UserDevice,
+                           cascade="all, delete",
+                           backref="user_device.user")
 
     def __init__(self, email: str):
         self.email = email
 
-    def __repr__(self) -> int:
+    def __repr__(self):
         return f"<User {self.email}>"
---
+++ backend/models/user_device.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from db.connection import Base
+
+
+class UserDevice(Base):
+    __tablename__ = "user_device"
+    id = Column(Integer, primary_key=True, index=True, nullable=True)
+    user = Column(Integer, ForeignKey('user.id'))
+    device = Column(String, nullable=False)
+
+    def __init__(self, user: int, device: str):
+        self.user = user
+        self.device = device
+
+    def __repr__(self):
+        return f"<UserDevice {self.id}>"
